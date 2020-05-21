c6fe2cc934b81861c15d52fab8c7211c04cf2a5a
Thomas Brown (Insight Global <v-thobr@microsoft.com>
2020-05-21 14:28:49 -0400

    update

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get("*", (req, res) => {
 });
 //}
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
