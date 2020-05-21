4b1a1af567cdd2981c16a87b340f13696586816e
Thomas Brown (Insight Global <v-thobr@microsoft.com>
2020-05-21 17:07:07 -0400

    fix

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.use("/api/keys", require("./routes/api/keys"));
 //app.use(express.static(path.join(__dirname, "build")));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+//if (process.env.NODE_ENV === "production") {
+// Set static folder
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+//}
 
 const PORT = process.env.PORT || 5000;
 
